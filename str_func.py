48608611a9c9fefb21827521410ac941d9729f94
Sukuna069 <127842980+Sukuna069@users.noreply.github.com>
2023-07-23 14:43:26 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def upper_func(text: str) -> str:
     """Переводит все буквы входящей строки в верхний регистр"""
     return text.upper()
+
+def title_func(text: str) -> str:
+    """Делает заглавными первые буквы каждого слова в строке, поступившей на вход"""
+    return text.title()
